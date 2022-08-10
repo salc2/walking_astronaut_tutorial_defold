e63c029dbbd66609fe4fb7d3b62c79d81c93f29b
Javier Salcedo <javier.salcedo@disneystreaming.com>
2022-08-10 23:21:43 +0100

    disparos

---
+++ Walking astronaut tutorial/main/gun.go
@@ -0,0 +1,30 @@
+components {
+  id: "gun"
+  component: "/main/gun.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "factory"
+  component: "/main/factory.factory"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
